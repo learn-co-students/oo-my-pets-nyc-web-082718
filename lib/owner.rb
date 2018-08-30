8cdda66da8406562cf40325e853d459ce40d32e6
github username <github email address>
2018-08-30 16:24:44 -0400

    practice object relations

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,85 @@
+require 'pry'
+require_relative './cat'
+require_relative './dog'
+require_relative './fish'
+
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+  @@count = 0
+
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = 'human'
+
+    @@all << self
+    @@count += 1
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(fish)
+    self.pets[:fishes] << Fish.new(fish)
+  end
+
+  def buy_cat(cat)
+    self.pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    self.pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    self.pets[:cats].map do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].map do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    #Owner sells all of its pets
+    #each pet mood changes to nervous
+    self.pets.each do |type, pet_array|
+      pet_array.map do |pet_obj|
+        pet_obj.mood = "nervous"
+      end
+    end
+    self.pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@all = []
+    @@count = 0
+  end
+
 end
